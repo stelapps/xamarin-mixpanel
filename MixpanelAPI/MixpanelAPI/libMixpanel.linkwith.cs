abfdd4fc3799941d3cb3b1e79ffbcb0063897435
folex <0xdxdy@gmail.com>
2013-09-13 12:21:49 +0400

    Made AdSupport.framework optional, now bindings work on iOS 5.0 and 5.1

--- MixpanelAPI/MixpanelAPI/libMixpanel.linkwith.cs
+++ MixpanelAPI/MixpanelAPI/libMixpanel.linkwith.cs
@@ -2,4 +2,5 @@ using System;
 using MonoTouch.ObjCRuntime;
 
 [assembly: LinkWith ("libMixpanel.a", LinkTarget.Simulator | LinkTarget.ArmV7, 
-                     Frameworks = "Foundation, AdSupport, CoreTelephony, UIKit, SystemConfiguration", ForceLoad = true)]
+                     Frameworks = "Foundation, CoreTelephony, UIKit, SystemConfiguration", 
+                     WeakFrameworks = "AdSupport", ForceLoad = true)]
